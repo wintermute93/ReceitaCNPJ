02663ea7a2f5ebfbc7dae9950f02e2ef6be0ed29
isaias.alves <isaias.alves@stone.com.br>
2023-05-31 12:31:20 -0300

    Added download of Cnaes.zip file and updated extraction logic

--- baixaCNPJ.py
+++ baixaCNPJ.py
@@ -16,6 +16,7 @@ if not os.path.exists("base/estabelecimentos"):
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 log_file = open(f"base/log_{timestamp}.txt", "w")
 
+# Download Estabelecimentos ZIP files
 for i in range(10):
     url = f"https://dadosabertos.rfb.gov.br/CNPJ/Estabelecimentos{i}.zip"
     response = requests.get(url, stream=True)
@@ -30,12 +31,33 @@ for i in range(10):
     log_file.write(f"Downloaded Estabelecimentos{i}.zip to base directory at {datetime.datetime.now()}\n")
     print(f"Downloaded Estabelecimentos{i}.zip to base directory")
 
-    # extract files to estabelecimentos directory inside base
+# Download Cnaes ZIP file
+cnaes_url = "https://dadosabertos.rfb.gov.br/CNPJ/Cnaes.zip"
+cnaes_response = requests.get(cnaes_url, stream=True)
+cnaes_total_size = int(cnaes_response.headers.get("Content-Length", 0))
+cnaes_t = tqdm.tqdm(total=cnaes_total_size, unit='B', unit_scale=True, desc="Downloading Cnaes.zip")
+with open("base/Cnaes.zip", "wb") as cnaes_file:
+    for data in cnaes_response.iter_content(block_size):
+        cnaes_t.update(len(data))
+        cnaes_file.write(data)
+cnaes_t.close()
+log_file.write(f"Downloaded Cnaes.zip to base directory at {datetime.datetime.now()}\n")
+print("Downloaded Cnaes.zip to base directory")
+
+# Extract and move Estabelecimentos ZIP files
+for i in range(10):
     with zipfile.ZipFile(f"base/Estabelecimentos{i}.zip", "r") as zip_ref:
         zip_ref.extractall("base/estabelecimentos")
     os.remove(f"base/Estabelecimentos{i}.zip")
     log_file.write(
         f"Extracted and moved Estabelecimentos{i}.zip to base/estabelecimentos directory at {datetime.datetime.now()}\n")
     print(f"Extracted and moved Estabelecimentos{i}.zip to base/estabelecimentos directory")
+
+# Extract Cnaes ZIP file
+with zipfile.ZipFile("base/Cnaes.zip", "r") as cnaes_zip_ref:
+    cnaes_zip_ref.extractall("base")
+os.remove("base/Cnaes.zip")
+log_file.write(f"Extracted and moved Cnaes.zip to base directory at {datetime.datetime.now()}\n")
+print("Extracted and moved Cnaes.zip to base directory")
 
 log_file.close()
